150332dc8f4889a2d7c79831ef84bba5a1a27c1e
Charles Xian <xcy919641322@gmail.com>
2022-07-15 16:49:35 +0200

    third
    third

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,20 @@
 import Head from "next/head";
 import icon1 from "/Users/charlesxian/WebstormProjects/tonext/my-project/pics/iconweb.png";
 import icon from "../pics/iconweb.svg";
-import Navbar from "../components/Navbar.jsx";
+import Navbar from "../components/Navbar.js";
 import Image from "next/image";
 import DescriptionLists from "../components/DescriptionLists";
 import Loginin from "../components/LoginIn";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar />
-      <Loginin></Loginin>
-    </div>
-  );
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Navbar />
+            <Loginin></Loginin>
+        </div>
+    );
 }
