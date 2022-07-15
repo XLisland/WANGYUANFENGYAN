f55f19a99ebd51c73c0f741c39944033e006dc40
Charles Xian <xcy919641322@gmail.com>
2022-07-15 12:39:05 +0200

    sec

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
 /* ./pages/index.js               */
-import Head from 'next/head';
-import icon1 from '/Users/charlesxian/WebstormProjects/tonext/my-project/pics/iconweb.png'
-import icon from '../pics/iconweb.svg'
-import Navbar from '../components/Navbar.jsx';
+import Head from "next/head";
+import icon1 from "/Users/charlesxian/WebstormProjects/tonext/my-project/pics/iconweb.png";
+import icon from "../pics/iconweb.svg";
+import Navbar from "../components/Navbar.jsx";
 import Image from "next/image";
-import DescriptionLists from '../components/DescriptionLists'
-import Loginin from '../components/LoginIn'
+import DescriptionLists from "../components/DescriptionLists";
+import Loginin from "../components/LoginIn";
 
 export default function Home() {
   return (
-      <div>
-        <Head>
-          <title>Create Next App</title>
-          <link rel='icon' href='/favicon.ico' />
-        </Head>
-        <Navbar />
-          <Loginin></Loginin>
-      </div>
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <Loginin></Loginin>
+    </div>
   );
 }
