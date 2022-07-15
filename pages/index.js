4504c12ef56c353b5ca6cb2964ce13a7b0f7c1dd
Charles Xian <xcy919641322@gmail.com>
2022-07-15 17:10:35 +0200

    fourth

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 /* ./pages/index.js               */
 import Head from "next/head";
-import icon1 from "/Users/charlesxian/WebstormProjects/tonext/my-project/pics/iconweb.png";
 import icon from "../pics/iconweb.svg";
 import Navbar from "../components/Navbar.js";
-import Image from "next/image";
-import DescriptionLists from "../components/DescriptionLists";
 import Loginin from "../components/LoginIn";
 
 export default function Home() {
@@ -12,7 +9,7 @@ export default function Home() {
         <div>
             <Head>
                 <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel={icon} href="/favicon.ico" />
             </Head>
             <Navbar />
             <Loginin></Loginin>
