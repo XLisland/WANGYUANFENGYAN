e234a40007e7559ba9503c4d4ff5fda1c9fe5fad
Charles Xian <xcy919641322@gmail.com>
2022-07-17 01:21:40 +0200

    delete mobx

--- pages/Products.js
+++ pages/Products.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "../components/Navbar.js";
-import { makeAutoObservable } from "mobx";
 import MyComponent from "../components/Products.items";
 
 function Products() {
